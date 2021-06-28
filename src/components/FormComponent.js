61d2f96268c9e3fef56d54204192d498f6958403
Bang-ipin <ersajogja@gmail.com>
2021-06-28 08:01:43 +0700

    init form component

---
+++ src/components/FormComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { connect } from "react-redux";
+import { reduxForm,Field } from "redux-form";
+
+const FormComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default  connect()(FormComponent);
--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-// const store = createStore(reducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ));
-const store = createStore(reducer, compose(applyMiddleware(thunk)));
+const store = createStore(reducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ));
+// const store = createStore(reducer, compose(applyMiddleware(thunk)));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
